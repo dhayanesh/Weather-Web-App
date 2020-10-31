21c0238bedababb58abeadf4d66856cabca431c3
gituser26 <dhaya2698@gmail.com>
2020-11-01 00:22:05 +0530

    readyForHeroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -80,6 +82,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is up on port 3000.')
 })
